f55a7218e32ecaaa90076d6baeb0092d72f2114d
Buddy Agyin <agyin3@gmail.com>
2020-02-21 22:39:24 -0700

    update testing pathnames to ./database/..

--- knexfile.js
+++ knexfile.js
@@ -13,14 +13,14 @@ module.exports = {
   testing: {
     client: 'sqlite3',
     connection: {
-      filename: './data/test.db3',
+      filename: './database/test.db3',
     },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: './database/migrations',
     },
     seeds: {
-      directory: './data/seeds',
+      directory: './database/seeds',
     },
   },
 };
